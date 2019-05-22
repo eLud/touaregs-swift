9d6457d4bfdcdae03f88a8f24abfe1d0da54f75c
Ludovic Ollagnier <ludovic@ollagnier.me>
2019-05-22 18:07:00 +0200

    Basic model

--- Playgrounds/Resthotels.playground/Contents.swift
+++ Playgrounds/Resthotels.playground/Contents.swift
@@ -1,4 +1,60 @@
-import UIKit
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
+
+struct Restaurant {
+    let name: String
+    let adress: String
+    var nbTables: Int
+    let phone: String
+    let website: URL
+    let images: [URL]
+    var owner: Person
+    var type: Set<RestaurantType>
+
+    func book() {
+
+    }
+}
+
+enum RestaurantType: String {
+    case italian = "Italien"
+    case french
+    case american
+    case indian
+    case japanese
+    case chinese
+    case fastFood
+    case lebanese
+    case vegan
+}
+
+enum HotelRankink: Int {
+    case oneStar
+    case twoStars
+    case threeStars
+    case fourStars
+    case fiveStars
+}
+
+struct Hotel {
+    let name: String
+    let adress: String
+    var nbRooms: Int
+    let phone: String
+    let website: URL
+    let images: [URL]
+    var owner: Person
+    var ranking: HotelRankink
+    var haveSwimmingPool: Bool
+
+    func book() {
+
+    }
+}
 
 //Restos
     //nom
